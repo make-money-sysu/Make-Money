36559ce031095d1b367231b51fdeda4a0349383f
leungyukshing <1042467380@qq.com>
2019-03-13 01:26:41 +0800

    新建header

---
+++ front-end/make-money/src/components/public/header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">
+    <router-link id="link" to="/"><img id='logo' src="../../assets/logo.png"></router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'header'
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import '../../style/header'
+</style>
