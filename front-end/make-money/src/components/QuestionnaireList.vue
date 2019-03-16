7cd847516af08147f8471c4b62a3f9297cd18088
leungyukshing <1042467380@qq.com>
2019-03-16 11:43:04 +0800

    添加问卷页面逻辑

--- front-end/make-money/src/components/QuestionnaireList.vue
+++ front-end/make-money/src/components/QuestionnaireList.vue
@@ -25,19 +25,43 @@
         </li>
       </ul>
     </template>
+
+    <div class="list-bottom" v-if="qslist.length == 0 ? false : true">
+      <label><input type="checkbox" id="all-check" v-model="selectAll">全选</label>
+      <button @click="iterator = delItems(); iterator.next()">删除</button>
+    </div>
+    <div class="add-qs" v-if="qslist.length === 0">
+      <button class="add-btn" @click="$router.push({name: 'qsEdit', params: {num: 0}})">+&nbsp;&nbsp;新建问卷</button>
+    </div>
+    <div class="shadow" v-if="showDialog">
+      <div class="del-dialog">
+        <header>
+          <span>提示</span>
+          <span class="close-btn" @click="showDialog = false">X</span>
+        </header>
+        <p>{{info}}</p>
+        <div class="btn-box">
+          <button class="yes" @click="iterator.next();">确定</button>
+          <button @click="showDialog = false">取消</button>
+        </div>
+      </div>
+
+    </div>
     <router-view></router-view>
   </div>
 </template>
-
 <script>
   export default {
     name: 'qsList',
     data() {
       return {
+        showDialog: false,
+        info: '',
+        iterator: {},
         qslist: [{ 'num': 3,
             'title': '第三份问卷',
             'time': '2017-3-27',
-            'state': 'issueed',
+            'state': 'issued',
             'stateTitle': '已发布',
             'checked': false,
             'question': [
@@ -47,6 +71,128 @@
             ]
           }]
         }
+    },
+    mounted() {
+      this.qslist.forEach(item => {
+        let [year, month, day] = item.time.split('-')
+        if (year < new Date().getFullYear()) {
+          item.state = 'issued'
+          item.stateTitle = '已发布'
+        }
+        else if (year == new Date().getFullYear() && month < new Date().getMonth() + 1) {
+          item.state = 'issued'
+          item.stateTitle = '已发布'
+        }
+        else if (year == new Date().getFullYear() && month == new Date().getMonth() + 1 && day < new Date().getDate()) {
+          item.state = 'issued'
+          item.stateTitle = '已发布'
+        }
+        else {
+          item.state = ''
+          item.stateTitle = '未发布'
+        }
+      })
+    },
+    methods: {
+      showDialogMsg(info) {
+        this.showDialog = true;
+        this.info = info;
+      },
+      *delItem(num) {
+        yield this.showDialogMsg('确认要删除此问卷')
+
+        yield (() => {
+          let index = 0;
+          for (let length = this.qslist.length; index < length; index++) {
+            if (this.qslist[index].num == num) {
+              break;
+            }
+          }
+          this.qslist.splice(index, 1)
+          this.showDialog = false;
+        })();
+      },
+      *delItems() {
+        yield this.showDialogMsg('确认要删除选中的问卷？')
+
+        yield(() => {
+          this.showDialog = false;
+          if (this.selectAll) {
+            this.qslist = [];
+            return;
+          }
+
+          if (this.selectGroup == [])
+            return;
+
+          this.selectGroup.forEach(item => {
+            if (this.qslist.indexOf(item) > -1) {
+              this.qslist.splice(this.qslist.indexOf(item), 1);
+            }
+          })
+        })();
+      },
+      *edit(item) {
+        yield (() => {
+          if (item.state == 'noissue') {
+            this.showDialogMsg('确认要编辑？')
+          }
+          else {
+            this.showDialogMsg('只有未发布的问卷才能编辑');
+          }
+        })();
+
+        yield (() => {
+          if (item.state !== 'noissue') {
+            this.showDialog = false;
+          }
+          else {
+            this.showDialog = false;
+            this.$router.push({name: 'qsEdit', params: {num: item.num}})
+          }
+        })();
+      },
+      *watchData(item) {
+        yield (() => {
+          if (item.state == 'noissue') {
+            this.showDialogMsg('未发布的问卷无数据查阅')
+          }
+          else {
+            this.$router.push({name: 'qsData', params: {num: item.num}})
+          }
+        })();
+
+        yield this.showDialog = false;
+      }
+    },
+    computed: {
+      selectAll: {
+        get() {
+          return this.selectCount === this.qslist.length && this.selectCount !== 0;
+        },
+        set(value) {
+          this.qslist.forEach(item => {
+            item.checked = value;
+          });
+          return value;
+        }
+      },
+      selectCount() {
+        let i = 0;
+        this.qslist.forEach(item => {
+          if (item.checked)
+            i++;
+        });
+        return i
+      },
+      selectGroup() {
+        let group = [];
+        this.qslist.forEach(item => {
+          if (item.checked)
+            group.push(item)
+        });
+        return group;
+      }
     }
   }
 </script>
