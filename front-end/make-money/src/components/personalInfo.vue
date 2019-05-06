0ccbd31b97d733d936613cca24a45a4042f63d8c
dick20 <806179953@qq.com>
2019-05-06 23:49:00 +0800

    完成 个人信息页面personinfo

--- front-end/make-money/src/components/personalInfo.vue
+++ front-end/make-money/src/components/personalInfo.vue
@@ -1,8 +1,7 @@
 <template>
-  <div id="app">
-    personalInfo
-    <router-view></router-view>
-  </div>
+	<div id = "test">
+		<iframe width="1710px" height="717px" src="../../static/html/user_center.html" id = "frame1"/>
+	</div>
 </template>
 
 <script>
