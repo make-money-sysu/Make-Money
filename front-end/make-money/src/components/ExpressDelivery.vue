3406eed0f0260dccf6b37a63f647674b5e17f748
Alva <liangjh45@mail2.sysu.edu.cn>
2019-05-31 17:16:28 +0800

    Express-Delivery-1

---
+++ front-end/make-money/src/components/ExpressDelivery.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="diliveryTable">
+		Hello World!
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'ExpressDelivery'
+	}
+</script>
+
+<style lang="css">
+	@import '../style/ExpressDelivery';
+</style>
